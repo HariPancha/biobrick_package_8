a8342c74a9f371f851beba9505f543314b851cea
HariPancha <harini.panchapakesan@igem.se>
2023-04-15 12:51:19 +0000

    fix

---
+++ myFunction.R
@@ -0,0 +1,14 @@
+#' Title
+#'
+#' @param foo
+#' @param bar
+#'
+#' @export
+#'
+myFunction <- function(foo, bar){
+  # Do stuff with foo and bar
+  foobar <- (foo * bar) / (foo + bar)
+  return(foobar)
+}
+
+
