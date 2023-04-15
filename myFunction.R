def81cabc0a28e6474bae5a6ea60168e5c97433a
anna9812 <annabell.hannibalsen@gmail.com>
2023-04-15 12:25:09 +0000

    set up

---
+++ myFunction.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param foo
+#' @param bar
+#'
+#' @export
+#'
+myFunction <- function(foo, bar){
+  # Do stuff with foo and bar
+  foobar <- (foo * bar) / (foo + bar)
+  return(foobar)
+}
